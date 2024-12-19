55271f43469be27620406a3e8e1bc8b8f2a9e887
mohamedfrix <mohamed.frihaoui@ensia.edu.dz>
2024-12-20 00:37:47 +0100

    fix cors problem

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from leaderboard import LeaderBoard
 import json
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, Flask!"
+CORS(app, resources={r"/api/*": {"origins": "*"}})  # Allow all origins for all /api routes
 
 @app.route('/api/leaderboard', methods=['GET'])
 def leaderboard():
     leaderboard = LeaderBoard()
-    competition_name = request.args['competition_name']
-    return jsonify(leaderboard.get_kaggle_leaderboard(competition_name=competition_name))
+    competition_name = request.args.get('competition_name')  # Safer to use .get() instead of indexing directly
+    data = leaderboard.get_kaggle_leaderboard(competition_name=competition_name)
+    return jsonify(data)
+
+@app.route('/')
+def home():
+    return "Hello, Flask!"
+
 
 
 if __name__ == '__main__':
